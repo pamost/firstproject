657daa84b150a9b65c12d500648cf44a42484690
Pavel Mostovoy <p.a.mostovoy@gmail.com>
2017-11-20 22:43:07 +0600

    Forms part2

--- firstapp/admin.py
+++ firstapp/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from firstapp.models import PizzaShop, Pizza
+from firstapp.models import PizzaShop, Pizza, Order
 
 admin.site.register(PizzaShop)
 admin.site.register(Pizza)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['pizza', 'name', 'phone', 'date']
--- firstapp/models.py
+++ firstapp/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 
+
 class PizzaShop(models.Model):
-    name=models.CharField(max_length=30, verbose_name='Пиццерия')
+    name = models.CharField(max_length=30, verbose_name='Пиццерия')
     description = models.TextField(verbose_name='Описание')
     rating = models.FloatField(default=0, verbose_name='Рейтинг')
     url = models.URLField(verbose_name='Сайт пиццерии')
 
     class Meta:
-        verbose_name='Пиццерия'
-        verbose_name_plural='Пиццерии'
+        verbose_name = 'Пиццерия'
+        verbose_name_plural = 'Пиццерии'
 
     def __str__(self):
         return self.name
+
 
 class Pizza(models.Model):
     pizzashop = models.ForeignKey(PizzaShop, on_delete=models.CASCADE)
@@ -28,8 +30,13 @@ class Pizza(models.Model):
     def __str__(self):
         return self.name
 
+
 class Order(models.Model):
     pizza = models.ForeignKey(Pizza, verbose_name='Пицца')
     name = models.CharField(max_length=30, verbose_name='Имя')
     phone = models.CharField(max_length=30, verbose_name='Телефон')
     date = models.DateTimeField(auto_now_add=True, verbose_name='Дата')
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
